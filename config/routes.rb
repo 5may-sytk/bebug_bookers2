91f15ce2623cf28f9e71444c4684ad33eca2d0c1
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-31 14:46:43 +0000

    7まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  root to: "home#top"
-  get 'homes/about' => 'home#about', as:'about'
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about', as:'about'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
