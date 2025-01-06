e64d10513ba9661d46ab16ab4b2678b9df178658
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2025-01-06 09:11:24 +0000

    応用課題５改

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :followers, through: :active_relationships, source: :follower
+  has_many :followings, through: :passive_relationships, source: :followed
 
   has_one_attached :profile_image
 
@@ -24,11 +24,11 @@ class User < ApplicationRecord
   end
 
   def follow(user)
-    active_relationships.create(followed_id: user.id)
+    passive_relationships.create(followed_id: user.id)
   end
 
   def unfollow(user)
-    active_relationships.find_by(followed_id: user.id).destroy
+    passive_relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
