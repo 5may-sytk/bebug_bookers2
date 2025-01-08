7a54dc58a6324da36fd61b4a29283e02b8964c74
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2025-01-08 09:27:31 +0000

    6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.book_id = book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+    end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
     BookComment.find_by(book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    book_comment.destroy
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
